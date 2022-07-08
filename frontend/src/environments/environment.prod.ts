718470749c78622d6340040e08d5f3383f81ede6
Renan <souza.rnn@gmail.com>
2022-07-08 12:14:33 -0300

    Teste variaveis de ambiente no front

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
-  apiUrlBase: '',
-  clientId: '',
-  clientSecret: '',
-  urlBaseSignin: ''
+  apiUrlBase: '${process.env.API_URL_BASE}',
+  clientId: '${process.env.CLIENT_ID}',
+  clientSecret: '${process.env.CLIENT_SECRET}',
+  urlBaseSignin: '${process.env.API_URL_BASE_SIGNIN}',
 };
