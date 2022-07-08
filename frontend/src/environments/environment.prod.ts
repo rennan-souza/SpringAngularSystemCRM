4e4fa46e1bec65bb572c996e79b0fd4b23b45410
Renan <souza.rnn@gmail.com>
2022-07-08 12:47:13 -0300

    Testando pegar variaveis de ambiente do servidor

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,7 +1,8 @@
+
 export const environment = {
   production: true,
-  apiUrlBase: '${process.env.API_URL_BASE}',
-  clientId: '${process.env.CLIENT_ID}',
-  clientSecret: '${process.env.CLIENT_SECRET}',
-  urlBaseSignin: '${process.env.API_URL_BASE_SIGNIN}',
+  apiUrlBase: process.env.API_URL_BASE,
+  clientId: process.env.CLIENT_ID,
+  clientSecret: process.env.CLIENT_SECRET,
+  urlBaseSignin: process.env.API_URL_BASE_SIGNIN,
 };
