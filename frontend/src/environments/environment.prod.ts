5df68c9eee3fd6976d13243e390e6a2286018c56
Renan <souza.rnn@gmail.com>
2022-07-08 23:44:17 -0300

    Fix frontend pegando variaveis de ambiente do servidor

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,8 +1,8 @@
 
 export const environment = {
   production: true,
-  apiUrlBase: process.env["API_URL_BASE"],
-  clientId: process.env["CLIENT_ID"],
-  clientSecret: process.env["CLIENT_SECRET"],
-  urlBaseSignin: process.env["API_URL_BASE_SIGNIN"],
+  apiUrlBase: process.env["API_URL_BASE"] as string,
+  clientId: process.env["CLIENT_ID"] as string,
+  clientSecret: process.env["CLIENT_SECRET"] as string,
+  urlBaseSignin: process.env["API_URL_BASE_SIGNIN"] as string,
 };
